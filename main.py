144373c9d8240e8ff7478c98bd6483885e5883f5
Caio Grimm <caiogrimm@gmail.com>
2024-04-18 13:42:27 -0400

    Adicionando novas Labels e novos Buttons

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class Paintk():
 
         self.window = tk.Tk()
         self.window.title("Paintk")
-        self.window.minsize(width=720, height=500)
+        self.window.minsize(width=1000, height=500)
         self.window.resizable(0, 0) # type: ignore
 
         self.img_eraser = tk.PhotoImage(file="icons/eraser.png")
@@ -27,6 +27,36 @@ class Paintk():
         for i in self.colors:
             self.button_color = tk.Button(self.bar_menu, bg=i, width=5, height=2)
             self.button_color.pack(side="left")
+
+        self.text_brushs = tk.Label(self.bar_menu, text="  Brushs:  ", fg="white", bg="#3b3b3b")
+        self.text_brushs.pack(side="left")
+
+        self.pen_size = tk.Spinbox(self.bar_menu, from_=1, to=50)
+        self.pen_size.pack(side="left")
+
+        self.text_brushs = tk.Label(self.bar_menu, text="  Brushs:  ", fg="white", bg="#3b3b3b")
+        self.text_brushs.pack(side="left")
+
+        self.button_line = tk.Button(self.bar_menu, image=self.img_line, bd=0)
+        self.button_line.pack(side="left")
+        
+        self.button_oval = tk.Button(self.bar_menu, image=self.img_oval, bd=0)
+        self.button_oval.pack(side="left")
+
+        self.button_eraser = tk.Button(self.bar_menu, image=self.img_eraser, bd=0)
+        self.button_eraser.pack(side="left")
+
+        self.button_square = tk.Button(self.bar_menu, image=self.img_square, bd=0)
+        self.button_square.pack(side="left")
+
+        self.text_options = tk.Label(self.bar_menu,  text="  Options:  ", fg="white", bg="#3b3b3b")
+        self.text_options.pack(side="left")
+
+        self.button_new = tk.Button(self.bar_menu, image=self.img_new, bd=0)
+        self.button_new.pack(side="left")
+
+        self.button_save = tk.Button(self.bar_menu, image=self.img_save, bd=0)
+        self.button_save.pack(side="left")
 
         self.area_draw = tk.Canvas(self.window, height=470, bg="red")
         self.area_draw.pack(fill="both")
